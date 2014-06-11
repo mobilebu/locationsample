67fb08d73ed057e815686a987c15475589f3ebf3
nttdmobilebu <sekimizuk@nttdata.co.jp>
2014-06-11 10:14:08 +0900

    20140611_1

--- rule.js
+++ rule.js
@@ -20,7 +20,7 @@ var redis  = require('redis');
 var redisClient = redis.createClient();
 
 function getRulePoint (item_id,sub){
- var selectSql = "select X(local_center) AS lat ,Y(local_center) AS lng, local_distance from push_item where push_item_id = ?;";
+ var selectSql = "select X(local_center) AS lat ,Y(local_center) AS lng, local_distance, schedule_from, schedule_to from push_item where push_item_id = ? and CURTIME() between schedule_from and schedule_to ;";
  var getQuery = connection.query(selectSql,[item_id]);
 
   getQuery
@@ -30,67 +30,81 @@ function getRulePoint (item_id,sub){
 
    .on('result',function(rows){
    console.log("---DB SELECT RULE ITEM:[", item_id, "]---");
-   console.log("RULE_LAT:",rows.lat);
-   console.log("RULE_LNG:",rows.lng);
+   console.log("RULE_LAT      :",rows.lat);
+   console.log("RULE_LNG      :",rows.lng);
+   console.log("RULE_TIME_FROM:",rows.schedule_from);
+   console.log("RULE_TIME_TO  :",rows.schedule_to);
    console.log("-------------------------------");
-   var redisData = [];
-   var redisLat = [];
-   var redisLng = [];
-   redisClient.sort(sub,"ALPHA","DESC","LIMIT", "0", "2", function (err, replies) {
-   if (replies){
-    console.log("---REDIS SELECT USER IS:[", sub, "]---");
-    replies.forEach(function (reply,i){
-     console.log(" REPLY[",i,"]",reply);
-     redisData.push((reply).slice(26));
-     redisLat.push(JSON.parse((reply).slice(26)).lat);
-     redisLng.push(JSON.parse((reply).slice(26)).lng);
-    });
+   //RULE定義から、有効時間内かどうかの判定を行う
+   var now = new Date();
+   console.log("---CHECK SCHEDULE OF RULE---");
+   console.log("NOW TOIME IS:",now);
     console.log("-------------------------------");
-    var before = {
-     lat: redisLat[1],
-     lng: redisLng[1]
-    };
+    //redisから最新の2レコードを取得して、ユーザの移動判定を行う
+    var redisData = [];
+    var redisLat = [];
+    var redisLng = [];
+    redisClient.sort(sub,"ALPHA","DESC","LIMIT", "0", "2", function (err, replies) {
+    if (replies){
+     console.log("---REDIS SELECT USER IS:[", sub, "]---");
+     replies.forEach(function (reply,i){
+      console.log(" REPLY[",i,"]",reply);
+      redisData.push((reply).slice(26));
+      redisLat.push(JSON.parse((reply).slice(26)).lat);
+      redisLng.push(JSON.parse((reply).slice(26)).lng);
+     });
+     console.log("-------------------------------");
+     var before = {
+      lat: redisLat[1],
+      lng: redisLng[1]
+     };
+ 
+     var after = {
+      lat: redisLat[0],
+      lng: redisLng[0]
+     };
+ 
+     var userLocation = {
+      before: before,
+      after: after
+     };
+     console.log("---CHECK USER LOCATION---");
+     console.log("USER_BEFORE_GEO:",before);
+     console.log("USER_AFTER _GEO:",after);
+ 
+     console.log("USER_LAT:",after.lat);
+     console.log("USER_LNG:",after.lng);
+     console.log("-------------------------------");
+     if(location.locationCheck(userLocation) == "Change") {
 
-    var after = {
-     lat: redisLat[0],
-     lng: redisLng[0]
-    };
-
-    var userLocation = {
-     before: before,
-     after: after
-    };
-    console.log("---CHECK USER LOCATION---");
-    console.log("USER_BEFORE_GEO:",before);
-    console.log("USER_AFTER _GEO:",after);
-
-    console.log("USER_LAT:",after.lat);
-    console.log("USER_LNG:",after.lng);
-    console.log("-------------------------------");
-    if(location.locationCheck(userLocation) == "Change") {
-     var distance = getDistance(rows,after)
-     console.log("DISTANCE:",distance);
-     if (distance < rows.local_distance) {
-      // 先ほど宣言したトランスポートオブジェクトでメールを送信
-      smtpTransport.sendMail(mailOptions, function (error, response) {
-       if (error) {
-        console.log(error);
-       }else {
+      //Rule定義を参照し、登録位置とユーザの位置の距離を取得する
+      var distance = getDistance(rows,after)
+      console.log("DISTANCE:",distance);
+      if (distance < rows.local_distance) {
+       // トランスポートオブジェクトでメールを送信
+       console.log("---SEND MAIL SITUATION---");
+       smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
+         console.log(error);
+        }else {
          console.log("Message sent: " + response.message);
-       }
-      });
+         console.log("-------------------------------");
+        }
+       });
+      }else{
+       console.log("USER IS OUT OF RULE AREA");
+       console.log("-------------------------------");
+      }
      }else{
-      console.log("USER IS OUT OF RULE AREA");
+      console.log("USER LOCATION IS NOT CHANGE");
+      console.log("-------------------------------");
      }
-    }else{
-     console.log("USER LOCATION IS NOT CHANGE");
+    }else if(replies = null){
+     console.log("REDIS:0");
+    }else if (err) {
+     console.log("REDIS_ERR:",err);
     }
-   }else if(replies = null){
-    console.log("REDIS:0");
-   }else if (err) {
-    console.log("REDIS_ERR:",err);
-   }
-  });
+   });
  });
 };
 
